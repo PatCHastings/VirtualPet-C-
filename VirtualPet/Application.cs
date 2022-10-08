56c7d1a59b19b32549c1710712d64e4dd6c18453
PatCHastings <phastings82@gmail.com>
2022-10-07 23:14:26 -0400

    added more functions to game logic

--- VirtualPet/Application.cs
+++ VirtualPet/Application.cs
@@ -22,14 +22,14 @@ namespace VirtualPet
             Console.WriteLine("_________Welcome to Virtual Pet!_________");
             Console.WriteLine("\nPlease enter the name of your pet: \n");
             String petName = Console.ReadLine();
-            VirtualPet pet1 = new VirtualPet("Patches", 0, 0, 0);
+            VirtualPet pet1 = new VirtualPet(petName, 0, 0, 0);
 
             Console.WriteLine("Nice to met you, " + petName + "! Your new master awaits.");
             bool gameOn = true;
             pet1.status();
             while (gameOn == true)
             {       
-                Console.WriteLine("\nNow, what would you like to do with your new pet " + petName + "?\n");
+                Console.WriteLine("\nNow, what would you like to do with your pet " + petName + "?\n");
                 Console.WriteLine("1: Feed it\n2: give it water\n3: Play with it\n4: quit game");
                 int prompt = int.Parse(Console.ReadLine());            
             
--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -25,26 +25,26 @@ namespace VirtualPet
 
         public void feed()
         {
-            hungerLevel = hungerLevel - 2;
+            hungerLevel = hungerLevel - 4;
             if (hungerLevel < 0)
             {
-                hungerLevel = 0;
+                hungerLevel = -2;
             }
         }
         public void water()
         {
-            thirstLevel = thirstLevel - 2;
+            thirstLevel = thirstLevel - 4;
             if (thirstLevel < 0)
             {
-                thirstLevel = 0;
+                thirstLevel = -2;
             }
         }
         public void play()
         {
-            boredomLevel = boredomLevel - 2;
+            boredomLevel = boredomLevel - 4;
             if (boredomLevel < 0)
             {
-                boredomLevel = 0;
+                boredomLevel = -2;
             }
         }
 
@@ -58,6 +58,40 @@ namespace VirtualPet
         public void status()
         {
             Console.WriteLine("\nHunger: " + hungerLevel + " Thirst: " + thirstLevel + " boredom: " + boredomLevel);
+            if (hungerLevel >= 10)
+            {
+                Console.WriteLine(petName + " is about to starve! You better Feed it!");
+            }
+            if (thirstLevel >= 10)
+            {
+                Console.WriteLine(petName + " is about to dehydrate! get it some water!");
+            }
+            if (boredomLevel >= 10)
+            {
+                Console.WriteLine(petName + " is about to run away! You better play with it!");
+            }
+            if(hungerLevel > 12)
+            {
+                Console.WriteLine("RIP: " + petName + " died of starvation..");
+                VirtualPet.gameOver();
+            }
+            if (hungerLevel > 12)
+            {
+                Console.WriteLine("I am sorry, " + petName + " died of dehydration..");
+                VirtualPet.gameOver();
+            }
+            if (hungerLevel > 12)
+            {
+                Console.WriteLine("uh oh, " + petName + " ran away and found a better master..");
+                VirtualPet.gameOver();
+            }
+
+        }
+
+        public static void gameOver()
+        {
+            Console.WriteLine("\n_____!!!!GAME OVER!!!!!_____\n");
+            Environment.Exit(0);
         }
     }
 }
